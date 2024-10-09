bdc7b0901a04e907a3efe2b43ac50eacf2d11a75
Spax <83354374+SpiritAxolotl@users.noreply.github.com>
2024-10-09 00:09:39 -0600

    update for the fetch event to be handled correctly

--- worker.js
+++ worker.js
@@ -1,27 +1,25 @@
 const domain = `https://random.spax.zone/webring-members`;
 
-const fetchJSON = async (url) => {
-  let data = [];
-  await fetch(url)
-    .then(response => response.json())
-    .then(d => data = d);
-  return data;
-};
-
 const randomArrayValue = (arr) => {
   return arr[Math.floor(arr.length * Math.random())];
 };
 
-const cobalt = await fetchJSON(`${domain}/cobalt.json`);
-const cohost = await fetchJSON(`${domain}/cohost.json`);
-
 export default {
   async fetch(request, env, ctx) {
+    const fetchJSON = async (url) => {
+      let data = [];
+      await fetch(url)
+        .then(response => response.json())
+        .then(d => data = d);
+      return data;
+    };
     const url = new URL(request.url);
     switch (url.pathname) {
       case "/webrings/cobalt":
+        const cobalt = await fetchJSON(`${domain}/cobalt.json`);
         return Response.redirect(randomArrayValue(cobalt), 302);
       case "/webrings/cohost":
+        const cohost = await fetchJSON(`${domain}/cohost.json`);
         return Response.redirect(randomArrayValue(cohost), 302);
       default:
         return Response.redirect("https://spax.zone/404", 302);
